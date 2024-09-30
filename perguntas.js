e34e19fc9d7ec09804426be37691481d85eb6fe0
Thurr77 <144943107+Thurr77@users.noreply.github.com>
2024-09-30 11:45:43 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'Copa',
+    'Quando o Brasil ganhou a copa?',
+    'foi em 1958, com Pelé fezendo dois gols com apenas 17 anos.'
+)
+
+criaCartao(
+    'Camisa 10',
+    'Quem foi o Rei Pelé?',
+    'Edson Arantes do Nascimento, mais conhecido como Pelé, foi um futebolista brasileiro que atuou como atacante. Descrito como o "Rei do Futebol", é amplamente considerado como o maior atleta de todos os tempos.'
+)
+
+criaCartao(
+    'R10',
+    'Queum foi os melhores dribladores da seleção?',
+    'Um dos melhores foi o Ronaldinho R10, junto com Garrincha e neymar.'
+)
+
+criaCartao(
+    'Seleção atual',
+    'Como fica a situação do Brasil nas eliminatórias da Copa de 2024?',
+    'Com dez pontos conquistados em oito partidas nas Eliminatórias da Copa, o Brasil é o 5º colocado na tabela e está a oito pontos atrás da Argentina, que lidera as classificatórias com 18 pontos.'
+)
